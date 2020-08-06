2df5218fe9522b0b5c55e4cb02a1db844afdbef4
Chester Heng <chester.heng@gmail.com>
2020-08-06 15:43:27 +0800

    01-React and JSX setup-Bootstrap setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 const reactElement = React.createElement("div", null, "Hello World");
 const domElement = document.getElementById("root");
