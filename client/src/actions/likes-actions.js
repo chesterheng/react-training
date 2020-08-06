a37549ba11276ec43a9634224e74f71752999136
Chester Heng <chester.heng@gmail.com>
2020-08-06 22:02:45 +0800

    05-REST Architecture-Handle likes & dislikes

--- client/src/actions/likes-actions.js
+++ client/src/actions/likes-actions.js
@@ -1,15 +1,27 @@
+import axios from "axios";
+
 export const DO_LIKE = "DO_LIKE";
-export const doLike = id => {
-  return {
-    type: DO_LIKE,
-    payload: id
-  };
+export const doLike = id => async dispatch => {
+  try {
+    await axios.post(`/rest/rules/${id}/likes`);
+    dispatch({
+      type: DO_LIKE,
+      payload: id
+    })
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const DO_DISLIKE = "DO_DISLIKE";
-export const doDislike = id => {
-  return {
-    type: DO_DISLIKE,
-    payload: id
-  };
+export const doDislike = id => async dispatch => {
+  try {
+    await axios.post(`/rest/rules/${id}/dislikes`);
+    dispatch({
+      type: DO_DISLIKE,
+      payload: id
+    })
+  } catch (error) {
+    console.log(error);
+  }
 };
