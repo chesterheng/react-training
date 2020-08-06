fe1a4d6edc216dd982152d8cd54c6f1aa8f5e08e
Chester Heng <chester.heng@gmail.com>
2020-08-06 17:12:45 +0800

    03-Tests-First test for Rule component

---
+++ client/src/__tests__/Rule.test.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Rule from "../Rule";
+import rules from "../data.json";
+
+describe("Rule", () => {
+  let rule;
+  let getByText;
+  let getByTitle;
+
+  beforeEach(() => {
+    rule = rules[0];
+    ({ getByText, getByTitle } = render(<Rule rule={rule} />));
+  });
+
+  test("should render rule title", () => {
+    const titleElement = getByText(rule.title);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test("should render rule description", () => {
+    const descriptionElement = getByText(rule.description);
+    expect(descriptionElement).toBeInTheDocument();
+  });
+
+  test("should render rule tags", () => {
+    rule.tags.forEach(tag => {
+      const tagElement = getByText(tag);
+      expect(tagElement).toBeInTheDocument();
+    });
+  });
+
+  test("should render rule likes", () => {
+    const likesElement = getByTitle("+1");
+    expect(likesElement).toHaveTextContent(rule.likes);
+  });
+
+  test("should render rule dislikes", () => {
+    const dislikesElement = getByTitle("-1");
+    expect(dislikesElement).toHaveTextContent(rule.dislikes);
+  });
+
+  test("should hide description when clicking on title", () => {
+    const titleElement = getByText(rule.title);
+    fireEvent.click(titleElement);
+
+    const descriptionElement = getByText(rule.description);
+    expect(descriptionElement.parentNode).toHaveClass("hidden");
+  });
+
+  test("should show description back when clicking on title", () => {
+    const title = getByText(rule.title);
+    fireEvent.click(title);
+    fireEvent.click(title);
+
+    const description = getByText(rule.description);
+    expect(description.parentNode).not.toHaveClass("hidden");
+  });
+});
