9950958e86ed759c048d0b2c2e78e570e1a2c68e
Chester Heng <chester.heng@gmail.com>
2020-08-07 00:44:31 +0800

    06-Routing-Setup the router

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import RuleList from "./RuleList";
 import store from "./store/app-store";
 
 const reactElement = (
   <Provider store={store}>
-    <RuleList />
+    <BrowserRouter>
+      <Route path="/" component={RuleList} />
+    </BrowserRouter>
   </Provider>
 );
 const domElement = document.getElementById("root");
