8b9bcf99d55f090d24aa7576b41bfa493a7428ec
Chester Heng <chester.heng@gmail.com>
2020-08-06 16:30:43 +0800

    01-React and JSX setup-Externalize a component

---
+++ client/src/Rule.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Rule = ({ rule: { title, description, likes, dislikes, tags } }) => {
+  const newTags = (tags || []).map(tag => (
+    <span key={tag} className="badge">
+      {tag}
+    </span>
+  ));
+  return (
+    <div className="panel panel-primary">
+      <div className="panel-heading" role="presentation">
+        {title}
+        <i className="pull-right glyphicon glyphicon-chevron-down"></i>
+      </div>
+      <div className="panel-body">
+        <p>{description}</p>
+      </div>
+      <div className="panel-footer">
+        <div className="btn-toolbar">
+          {newTags}
+          <div className="btn-group btn-group-xs pull-right">
+            <button className="btn btn-primary" title="Update">
+              <i className="glyphicon glyphicon-pencil"></i>
+            </button>
+          </div>
+          <div className="btn-group btn-group-xs pull-right">
+            <button className="btn btn-default" title="+1">
+              {likes} <i className="glyphicon glyphicon-thumbs-up"></i>
+            </button>
+            <button className="btn btn-default" title="-1">
+              {dislikes} <i className="glyphicon glyphicon-thumbs-down"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Rule;
--- client/src/RuleList.js
+++ client/src/RuleList.js
@@ -1,43 +1,10 @@
 import React, { Fragment } from "react";
+import Rule from "./Rule";
 
 const RuleList = ({ rules }) => {
-  const newRules = (rules || []).map(rule => {
-    const { title, description, likes, dislikes, tags } = rule;
-    const newTags = (tags || []).map(tag => (
-      <span key={tag} className="badge">
-        {tag}
-      </span>
-    ));
-    return (
-      <div className="panel panel-primary">
-        <div className="panel-heading" role="presentation">
-          {title}
-          <i className="pull-right glyphicon glyphicon-chevron-down"></i>
-        </div>
-        <div className="panel-body">
-          <p>{description}</p>
-        </div>
-        <div className="panel-footer">
-          <div className="btn-toolbar">
-            {newTags}
-            <div className="btn-group btn-group-xs pull-right">
-              <button className="btn btn-primary" title="Update">
-                <i className="glyphicon glyphicon-pencil"></i>
-              </button>
-            </div>
-            <div className="btn-group btn-group-xs pull-right">
-              <button className="btn btn-default" title="+1">
-                {likes} <i className="glyphicon glyphicon-thumbs-up"></i>
-              </button>
-              <button className="btn btn-default" title="-1">
-                {dislikes} <i className="glyphicon glyphicon-thumbs-down"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  });
+  const newRules = (rules || []).map(rule => (
+    <Rule key={rule.id} rule={rule} />
+  ));
 
   return <Fragment>{newRules}</Fragment>;
 };
