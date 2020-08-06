7b851ccf1f852d5aa867bb6b8872d45e92243583
Chester Heng <chester.heng@gmail.com>
2020-08-06 19:10:06 +0800

    04-Redux-Likes and Dislikes-Reducers

--- client/src/reducers/__tests__/rules-reducer.test.js
+++ client/src/reducers/__tests__/rules-reducer.test.js
@@ -1,6 +1,7 @@
 import rules from "../../data.json";
 import reducer from "../rules-reducer";
 import { RULES_LOADED } from "../../actions/rules-actions";
+import { DO_LIKE, DO_DISLIKE } from "../../actions/likes-actions";
 
 describe("Rules reducer", () => {
   test("should return the initial state", () => {
@@ -23,4 +24,27 @@ describe("Rules reducer", () => {
     
     expect(newState).toEqual(expectedNewState);
   });
+
+  test("should increment likes", () => {
+    const action = {
+      type: DO_LIKE,
+      payload: 1
+    };
+    const previousState = rules;
+    const newState = reducer(previousState, action);
+
+    expect(newState[0].likes).toBe(1);
+  });
+
+  test("should increment dislikes", () => {
+    const action = {
+      type: DO_DISLIKE,
+      payload: 1
+    };
+    const previousState = rules;
+    const newState = reducer(previousState, action);
+
+    expect(newState[0].dislikes).toBe(1);
+  });
+
 });
--- client/src/reducers/rules-reducer.js
+++ client/src/reducers/rules-reducer.js
@@ -1,9 +1,32 @@
 import { RULES_LOADED } from "../actions/rules-actions";
+import { DO_LIKE, DO_DISLIKE } from "../actions/likes-actions";
 
 const rulesReducer = (state = [], action) => {
   switch (action.type) {
     case RULES_LOADED: {
       return action.payload;
+    }
+
+    case DO_LIKE: {
+      const index = state.findIndex(rule => rule.id === action.payload);
+      const newRule = {
+        ...state[index]
+      };
+      newRule.likes += 1;
+      const newRules = [...state];
+      newRules[index] = newRule;
+      return newRules;
+    }
+
+    case DO_DISLIKE: {
+      const index = state.findIndex(rule => rule.id === action.payload);
+      const newRule = {
+        ...state[index]
+      };
+      newRule.dislikes += 1;
+      const newRules = [...state];
+      newRules[index] = newRule;
+      return newRules;
     }
 
     default:
